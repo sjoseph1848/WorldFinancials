cb7bb5dbcf40499fa6859e8a4203c804a14d9a40
Sam <sjoseph1848@gmail.com>
2018-11-04 20:32:42 -0600

    Another update to the graph with numbers

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 //Chart Options
 const options = {
   chart: {
-    height: 650,
-    width: 650,
+    height: 350,
+    width: 500,
     type: 'bar',
     background: '#f4f4f4',
     forecolor: '#333'
@@ -10,21 +10,17 @@ const options = {
   series: [
     {
       name: 'Debt In 2008',
-      data: [100, 90, 80, 70, 60, 50, 40, 30, 20, 10]
+      data: [191.81, 109.42, 74.54, 72.85, 68.06, 64.93]
     }
   ],
   xaxis: {
     categories: [
-      'New York',
-      'Los Angeles',
-      'Chicago',
-      'Houston',
-      'Philadelphia',
-      'Phoenix',
-      'San Antonio',
-      'San Diego',
-      'Dallas',
-      'San Jose'
+      'Japan',
+      'Greece',
+      'India',
+      'United States',
+      'France',
+      'Germany'
     ]
   },
   plotOptions: {
